71ae201376623570bbe7eef9c1b4e55f4e48195e
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-28 22:42:22 +0100

    Create Global visa tracker
    Global Visa Tracker is a web application that helps users track their visa applications. Users can enter the details of their visa applications, track their current status, and manage them easily.

---
+++ script.js
@@ -0,0 +1,30 @@
+
+document.getElementById('visaForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+
+    var fullName = document.getElementById('fullName').value;
+    var passportNumber = document.getElementById('passportNumber').value;
+    var applicationDate = document.getElementById('applicationDate').value;
+    var status = document.getElementById('status').value;
+
+
+    var table = document.querySelector('#visaList table');
+    var row = table.insertRow(-1);
+    var fullNameCell = row.insertCell(0);
+    var passportNumberCell = row.insertCell(1);
+    var applicationDateCell = row.insertCell(2);
+    var statusCell = row.insertCell(3);
+
+
+    fullNameCell.innerHTML = fullName;
+    passportNumberCell.innerHTML = passportNumber;
+    applicationDateCell.innerHTML = applicationDate;
+    statusCell.innerHTML = status;
+
+
+    document.getElementById('fullName').value = '';
+    document.getElementById('passportNumber').value = '';
+    document.getElementById('applicationDate').value = '';
+    document.getElementById('status').value = '';
+});
