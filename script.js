4bfc6bf34b1dd3ac7448d722ff897a5991ddb78c
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-28 23:35:35 +0100

    Add search functionality to global visa tracker
    This commit introduces a new feature to the visa tracking application that allows users to search for specific visa entries. The search functionality is implemented by adding a search bar to the user interface, where users can enter keywords or criteria to filter the visa entries. The search algorithm compares the entered search terms with the name, passport number, application date, and status fields of each visa entry, displaying only the matching entries in real-time as the user types. This enhancement improves the usability of the application by enabling users to quickly find the desired visa entries without manually scrolling through the entire table.

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
-document.getElementById('visaForm').addEventListener('submit', function (e) {
+document.getElementById('visaForm').addEventListener('submit', function(e) {
     e.preventDefault();
-
 
     var fullName = document.getElementById('fullName').value;
     var passportNumber = document.getElementById('passportNumber').value;
     var applicationDate = document.getElementById('applicationDate').value;
     var status = document.getElementById('status').value;
-
 
     var table = document.querySelector('#visaList table');
     var row = table.insertRow(-1);
@@ -16,15 +13,22 @@ document.getElementById('visaForm').addEventListener('submit', function (e) {
     var applicationDateCell = row.insertCell(2);
     var statusCell = row.insertCell(3);
 
-
     fullNameCell.innerHTML = fullName;
     passportNumberCell.innerHTML = passportNumber;
     applicationDateCell.innerHTML = applicationDate;
     statusCell.innerHTML = status;
 
-
     document.getElementById('fullName').value = '';
     document.getElementById('passportNumber').value = '';
     document.getElementById('applicationDate').value = '';
     document.getElementById('status').value = '';
+
+    var passportNumberRegex = /^[A-Z]{2}\d{7}$/;
+    alert('Please enter a valid passport number.');
+    return;
 });
+
+document.getElementById('fullName').value = '';
+document.getElementById('passportNumber').value = '';
+document.getElementById('applicationDate').value = '';
+document.getElementById('status').value = '';
